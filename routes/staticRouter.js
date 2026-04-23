de8a5e379b59d3ad0cd2675caa035d3201c1c35d
UmerQazi17 <omerqazi1725@gmail.com>
2026-04-23 14:53:06 +0500

    Add authentication route changes

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -1,7 +1,20 @@
 import express from 'express';
+import URL from '../models/url.js';
 
 const router = express.Router();
-router.get("/", (req,res) => {
-  return res.render("home");
-})
+
+// router.get("/", async (req,res) => {
+//   const allUrls = await URL.find({});
+//   return res.render("home", {
+//     urls : allUrls
+//   });
+// })
+router.get("/", async (req, res) => {
+  const allUrls = await URL.find({});
+  console.log("allUrls:", allUrls);        // 👈 add this
+  console.log("allUrls length:", allUrls.length);  // 👈 and this
+  return res.render("home", {
+    urls: allUrls
+  });
+});
 export default router;
